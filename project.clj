2399ff64c999b54fb64dd5245d8b287b06579bb4
Krzysiek Herod <krzysiek.herod@gmail.com>
2016-06-15 19:25:07 +0200

    keep things up-to-date

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [honeysql "0.6.3"]
-                 [org.clojure/java.jdbc "0.5.8"]
+                 [honeysql "0.7.0"]
+                 [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4.1208"]])
